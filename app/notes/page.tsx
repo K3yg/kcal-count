dcfb45254060bcfb4833057cc64f3906cc020340
K3yg <eduardobrandt61@gmail.com>
2024-02-17 04:06:10 -0300

    initial commit

---
+++ app/notes/page.tsx
@@ -0,0 +1,20 @@
+import { createClient } from '@/utils/supabase/server';
+
+export default async function Notes() {
+  const supabase = createClient();
+  const { data: notes } = await supabase.from("notes").select();
+  
+ 
+
+
+  return (
+    <div>
+      { notes && notes.map((note) => (
+        <div key={note.id}>
+          <h2>{note.title}</h2>
+          <p>{note.content}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
